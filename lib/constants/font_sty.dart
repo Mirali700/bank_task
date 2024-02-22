bf8b2da7e92edd974571052d39d7d90681125c64
Mirali700 <miralialiyev2002@gmail.com>
2024-02-22 19:32:00 +0400

    22/02/2024

--- lib/constants/font_sty.dart
+++ lib/constants/font_sty.dart
@@ -2,4 +2,4 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 TextStyle kCardFont =
-    GoogleFonts.nunitoSans().copyWith(fontSize: 20, color: Colors.white);
+    GoogleFonts.nunito().copyWith(fontSize: 20, color: Colors.white);
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 import '../constants/font_sty.dart';
 
@@ -11,7 +12,7 @@ class HomeScreen extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: const Text('Gaffar Bank'),
+        title:  Text('Gaffar Bank',style: GoogleFonts.lobster(),),
         centerTitle: true,
         actions: [
           IconButton(
@@ -28,12 +29,12 @@ class HomeScreen extends StatelessWidget {
         child: Container(
           padding: const EdgeInsets.all(16),
           height: 250,
-          decoration: BoxDecoration(
-            borderRadius: const BorderRadius.all(Radius.circular(24)),
+          decoration: const BoxDecoration(
+            borderRadius: BorderRadius.all(Radius.circular(24)),
             gradient: LinearGradient(
               colors: [
-                Theme.of(context).colorScheme.primary,
-                Theme.of(context).colorScheme.secondary,
+               Color(0xff654ea3),
+               Color(0xffeaafc8),
               ],
             ),
           ),
@@ -53,13 +54,28 @@ class HomeScreen extends StatelessWidget {
               Column(
                 children: [
                   Row(
-                children: [
-                  Text('9999 9999 9999 9999', style: kCardFont),
-                  IconButton(
-                    style: const ButtonStyle(
-                        iconColor: MaterialStatePropertyAll(Colors.white)),
-                    onPressed: () {},
-                    icon: const Icon(Icons.copy),
+                    children: [
+                      Text('9999 9999 9999 9999', style: kCardFont),
+                      IconButton(
+                        style: const ButtonStyle(
+                            iconColor: MaterialStatePropertyAll(Colors.white)),
+                        onPressed: () {},
+                        icon: const Icon(Icons.copy),
+                      ),
+                    ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        '12/24',
+                        style: kCardFont,
+                      ),
+                      Text(
+                        'CVV',
+                        style: kCardFont,
+                      ),
+                    ],
                   ),
                 ],
               ),
@@ -67,23 +83,12 @@ class HomeScreen extends StatelessWidget {
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
                   Text(
-                    '12/24',
+                    'Javid Gaffar',
                     style: kCardFont,
                   ),
-                  Text(
-                    'CVV',
-                    style: kCardFont,
-                  ),
-                ],
-              ),
-                ],
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text('Javid Gaffar',style: kCardFont,),
-                  const FaIcon(FontAwesomeIcons.ccVisa,
-                  color: Colors.white,
+                  const FaIcon(
+                    FontAwesomeIcons.ccVisa,
+                    color: Colors.white,
                   ),
                 ],
               ),
